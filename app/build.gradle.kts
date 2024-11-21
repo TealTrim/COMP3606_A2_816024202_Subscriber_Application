22b1ca06977e470f7b5619dd046b5288387a4fb6
TealTrim <teal.trim20@gmail.com>
2024-11-20 23:42:15 -0400

    Final Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
 }
 
 android {
+    packagingOptions { resources.excludes.add("META-INF/*") }
     namespace = "com.example.comp3606_a2_816024202_sub_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.comp3606_a2_816024202_sub_app"
@@ -33,15 +34,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    //implementation(libs.hivemq.mqtt.client)
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/comp3606_a2_816024202_sub_app/MainActivity.kt
+++ app/src/main/java/com/example/comp3606_a2_816024202_sub_app/MainActivity.kt
@@ -1,3 +1,15 @@
+/*
+-------------------------------------------
+Student Information:
+-------------------------------------------
+Student Name: Teal Trim
+Student ID: 816024202
+Course: Wireless and Mobile Computing (COMP3606)
+Assignment: COMP3606 Assignment 2
+Date: 20/11/2024
+-------------------------------------------
+*/
+
 package com.example.comp3606_a2_816024202_sub_app
 
 import android.os.Bundle
@@ -5,9 +17,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback
+{
+    //Subscriber Application: (Map Stuff)
+    //Attributes:
+    private var client: Mqtt5BlockingClient? = null
+    private lateinit var mMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +38,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
